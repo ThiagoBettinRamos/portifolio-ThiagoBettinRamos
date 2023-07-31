cfb5ad659b9e928d4f9cf8f2f71338bf5a6f5039
ThiagoBettinRamos <thiago_bettin@hotmail.com>
2023-07-31 19:41:42 -0300

    atualização

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
-  facebookUser: "",
+  nameUser: "Thiago Bettin Ramos",
+  githubUser: "ThiagoBettinRamos",
+  linkedinUser: "Thiago Bettin Ramos",
+  whatsappNumber: "+5512991227400",
+  emailUser: "thiago_bettin@hotmail.com",
+  instagramUser: "thiagobettin21",
+  facebookUser: "Thiago Bettin Ramos",
 };
